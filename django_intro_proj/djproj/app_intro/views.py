256cd676bcd153d741ac074b75aaaeb9b5d7b5ad
Luis Souza <luisttine@gmail.com>
2021-07-22 22:29:36 -0300

    first test

--- django_intro_proj/djproj/app_intro/views.py
+++ django_intro_proj/djproj/app_intro/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
 
 def print_reqs(request):
     print (request)
     return HttpResponse('hello world')
+
+@csrf_exempt
+def print_reqs(request):
+    print ("\n-------META--------------\n")
+    print(request.META)
+    print ("\n-------REQUEST--------------\n")
+    print (request)
+    print ("\n-------REQUEST POST--------------\n")
+    print (request.POST)
+    print ("\n-------REQUEST GET--------------\n")
+    print (request.GET)
+return HttpResponse('olá mundo')
+
+def index(request):
+    template_name = 'teste.html'
+
+return render(resquest, template_name)
